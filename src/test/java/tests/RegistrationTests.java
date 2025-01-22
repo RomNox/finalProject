a493e349f58cd752cf3377952c143960fedaa01c
monk-admin <denlee8348@gmail.com>
2025-01-22 14:15:52 +0100

    Adding new tests

--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,4 +1,84 @@
 package tests;
 
-public class RegistrationTests {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class RegistrationTests extends TestBase{
+
+    @Test
+    public void newUserRegistrationPosTest(){
+
+        //click on sign up button
+
+        driver.findElement(By.cssSelector("[href='#/register']")).click();
+        //enter first name
+        driver.findElement(By.xpath("//input[@id='firstName']")).click();
+        driver.findElement(By.xpath("//input[@id='firstName']")).clear();
+        driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("Alex");
+        //enter last name
+        driver.findElement(By.xpath("//input[@id='lastName']")).click();
+        driver.findElement(By.xpath("//input[@id='lastName']")).clear();
+        driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Pereira");
+        //enter email
+        driver.findElement(By.xpath("//input[@id='email']")).click();
+        driver.findElement(By.xpath("//input[@id='email']")).clear();
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("pereira@gm.com");
+        //enter password
+        driver.findElement(By.xpath("//input[@id='password']")).click();
+        driver.findElement(By.xpath("//input[@id='password']")).clear();
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Pereira123!");
+        //check checkboxes
+        driver.findElement(By.xpath("//input[@id='consent']")).click();
+        driver.findElement(By.xpath("//input[@id='policy']")).click();
+        //click on create account
+        driver.findElement(By.xpath("//button[contains(text(),'Create account')]")).click();
+        //verify that Welcome is present
+        Assert.assertTrue(isElementPresent(By.xpath("//h2[contains(text(),'Welcome')]")));
+
+    }
+
+    @Test
+    public void existedUserRegistrationNegTest(){
+
+        driver.findElement(By.cssSelector("[href='#/register']")).click();
+
+        driver.findElement(By.xpath("//input[@id='firstName']")).click();
+        driver.findElement(By.xpath("//input[@id='firstName']")).clear();
+        driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("Alex");
+
+        driver.findElement(By.xpath("//input[@id='lastName']")).click();
+        driver.findElement(By.xpath("//input[@id='lastName']")).clear();
+        driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Pereira");
+
+        driver.findElement(By.xpath("//input[@id='email']")).click();
+        driver.findElement(By.xpath("//input[@id='email']")).clear();
+        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("leeden585@gmail.com");
+
+        driver.findElement(By.xpath("//input[@id='password']")).click();
+        driver.findElement(By.xpath("//input[@id='password']")).clear();
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Pereira123!");
+
+        driver.findElement(By.xpath("//input[@id='consent']")).click();
+        driver.findElement(By.xpath("//input[@id='policy']")).click();
+
+        driver.findElement(By.xpath("//button[contains(text(),'Create account')]")).click();
+
+        Assert.assertTrue(isElementPresent(By.xpath("//div[contains(text(),'An error occurred during registration. You may have entered an existing email.')]")));
+
+    }
+
+    public boolean isAlertDisplayed(){
+        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.alertIsPresent());
+        if (alert == null){
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,4 +1,35 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
 public class TestBase {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.get("https://pets-care-u2srs.ondigitalocean.app");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown(){
+        driver.quit();
+    }
+
+    public boolean isHomeComponentPresent(){
+        return driver.findElements(By.xpath("//h1[contains(text(),'Welcome to Pet Service')]")).size()>0;
+    }
+
+    public boolean isElementPresent(By locator){
+        return driver.findElements(locator).size()>0;
+    }
 }
