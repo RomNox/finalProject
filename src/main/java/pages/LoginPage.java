fb72e18329ea3bc066493f21c2b86b1f5f1791f8
monk-admin <denlee8348@gmail.com>
2025-01-23 21:50:36 +0100

    RegistrationTests

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,30 @@
 package pages;
 
-public class LoginPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+public class LoginPage extends BasePage{
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(xpath = "//h2[contains(text(),'Welcome')]")
+    WebElement welcomeText;
+
+    public LoginPage verifySuccessRegistration(String text) {
+        Assert.assertTrue(welcomeText.getText().contains(text));
+        return this;
+    }
+
+    @FindBy(xpath = "//div[contains(text(),'An error occurred during registration. You may have entered an existing email.')]")
+    WebElement warningMessage;
+
+    public LoginPage verifyMessageOfExistedUser(String text) {
+        Assert.assertTrue(warningMessage.getText().contains(text));
+        return this;
+    }
+
 }
--- src/test/java/tests/RegistrationTests.java
+++ src/test/java/tests/RegistrationTests.java
@@ -1,84 +1,33 @@
 package tests;
 
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
+import pages.HomePage;
+import pages.LoginPage;
+import pages.RegistrationPage;
 
 public class RegistrationTests extends TestBase{
 
-    @Test
-    public void newUserRegistrationPosTest(){
-
-        //click on sign up button
-
-        driver.findElement(By.cssSelector("[href='#/register']")).click();
-        //enter first name
-        driver.findElement(By.xpath("//input[@id='firstName']")).click();
-        driver.findElement(By.xpath("//input[@id='firstName']")).clear();
-        driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("Alex");
-        //enter last name
-        driver.findElement(By.xpath("//input[@id='lastName']")).click();
-        driver.findElement(By.xpath("//input[@id='lastName']")).clear();
-        driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Pereira");
-        //enter email
-        driver.findElement(By.xpath("//input[@id='email']")).click();
-        driver.findElement(By.xpath("//input[@id='email']")).clear();
-        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("pereira@gm.com");
-        //enter password
-        driver.findElement(By.xpath("//input[@id='password']")).click();
-        driver.findElement(By.xpath("//input[@id='password']")).clear();
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Pereira123!");
-        //check checkboxes
-        driver.findElement(By.xpath("//input[@id='consent']")).click();
-        driver.findElement(By.xpath("//input[@id='policy']")).click();
-        //click on create account
-        driver.findElement(By.xpath("//button[contains(text(),'Create account')]")).click();
-        //verify that Welcome is present
-        Assert.assertTrue(isElementPresent(By.xpath("//h2[contains(text(),'Welcome')]")));
-
+    @BeforeMethod
+    public void precondition(){
+        new HomePage(driver).selectSignUp();
     }
 
     @Test
-    public void existedUserRegistrationNegTest(){
-
-        driver.findElement(By.cssSelector("[href='#/register']")).click();
-
-        driver.findElement(By.xpath("//input[@id='firstName']")).click();
-        driver.findElement(By.xpath("//input[@id='firstName']")).clear();
-        driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("Alex");
-
-        driver.findElement(By.xpath("//input[@id='lastName']")).click();
-        driver.findElement(By.xpath("//input[@id='lastName']")).clear();
-        driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Pereira");
-
-        driver.findElement(By.xpath("//input[@id='email']")).click();
-        driver.findElement(By.xpath("//input[@id='email']")).clear();
-        driver.findElement(By.xpath("//input[@id='email']")).sendKeys("leeden585@gmail.com");
-
-        driver.findElement(By.xpath("//input[@id='password']")).click();
-        driver.findElement(By.xpath("//input[@id='password']")).clear();
-        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("Pereira123!");
-
-        driver.findElement(By.xpath("//input[@id='consent']")).click();
-        driver.findElement(By.xpath("//input[@id='policy']")).click();
-
-        driver.findElement(By.xpath("//button[contains(text(),'Create account')]")).click();
-
-        Assert.assertTrue(isElementPresent(By.xpath("//div[contains(text(),'An error occurred during registration. You may have entered an existing email.')]")));
-
+    public void newUserRegistrationPositiveTest(){
+        new RegistrationPage(driver)
+                .enterUserData("Alex", "Pereira", "pereira1@gmail.com", "Pereira123!")
+                .checkBoxes()
+                .clickOnCreateAccountButton();
+        new LoginPage(driver).verifySuccessRegistration("Welcome");
     }
 
-    public boolean isAlertDisplayed(){
-        Alert alert = new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.alertIsPresent());
-        if (alert == null){
-            return false;
-        } else {
-            return true;
-        }
+    @Test
+    public void newUserRegistrationNegativeTest(){
+        new RegistrationPage(driver)
+                .enterUserData("Alex", "Pereira", "pereira@gmail.com", "Pereira123!")
+                .checkBoxes()
+                .clickOnCreateAccountButton()
+                .verifyMessageOfExistedUser("An error occurred during registration. You may have entered an existing email.");
     }
 }
