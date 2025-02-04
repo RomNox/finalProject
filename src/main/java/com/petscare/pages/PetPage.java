df357d7706d57dea00f519bfc2f6cc9f93651289
Kseniia156 <hedyrover@gmail.com>
2025-02-04 15:48:57 +0100

    add PetPage and PetTests

--- src/main/java/com/petscare/pages/PetPage.java
+++ src/main/java/com/petscare/pages/PetPage.java
@@ -1,9 +1,73 @@
 package com.petscare.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class PetPage extends BasePage{
+
     public PetPage(WebDriver driver) {
         super(driver);
     }
+
+
+    @FindBy(xpath = "//button[contains(text(),'My Pets')]")
+    WebElement myPetButton;
+
+    public PetPage clickMyPetButton() {
+        click(myPetButton);
+        return this;
+
+    }
+
+    @FindBy (xpath = "//span[contains(text(),'Add a New Pet')]")
+    WebElement myAddNewPet;
+
+
+    public PetPage clickOnAddNewPet() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(myAddNewPet)).click();
+        return this;
+    }
+
+    @FindBy(xpath = "//input[@name='name']")
+    WebElement petNameField;
+
+    public PetPage namePet(String name) {
+
+        type(petNameField,name);
+        return  this;
+
+    }
+
+    @FindBy(xpath = "//select[@name='type']")
+    WebElement selectType;
+
+    public PetPage selectTypeCategory() {
+        new Select(selectType).selectByValue("cat");
+        return this;
+    }
+
+    @FindBy(xpath = "//button[contains(text(),'Save')]")
+    WebElement saveButton;
+
+    public PetPage clickSaveButton() {
+        click(saveButton);
+        return this;
+    }
+
+    @FindBy(xpath = "//button[contains(text(),'Delete Pet')]")
+    WebElement deletePetButton;
+
+    public PetPage clickDeleteButton() {
+        click(deletePetButton);
+        return this;
+    }
+
+
 }
--- src/test/java/com/petscare/tests/PetTests.java
+++ src/test/java/com/petscare/tests/PetTests.java
@@ -1,4 +1,40 @@
 package com.petscare.tests;
 
-public class PetTests {
+import com.petscare.pages.HomePage;
+import com.petscare.pages.LoginPage;
+import com.petscare.pages.PetPage;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PetTests extends TestBase {
+
+    // Авторизация
+    @BeforeMethod
+    public void precondition() {
+        new HomePage(driver).selectLogIn();
+        new LoginPage(driver).login("kseniia987@gm.com", "Kseniia987@");
+    }
+
+    // Тест на добавление питомца
+    @Test
+    public void testAddNewPet() {
+        new PetPage(driver)
+                .clickMyPetButton()
+                .clickOnAddNewPet()
+                .namePet("Mars")
+                .selectTypeCategory()
+                .clickSaveButton();
+
+    }
+
+    //Тест на удаление питомца
+    @Test
+    public void deletePetTest() {
+        new PetPage(driver)
+                .clickMyPetButton()
+                .clickDeleteButton();
+
+
+    }
+
 }
